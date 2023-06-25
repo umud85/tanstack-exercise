c70e5986fe2739a0f15eee73dbed60e45f5b3e15
Umut Akbayin <umutakbayin@posteo.de>
2023-06-25 18:34:17 +0200

    add basic tests

--- __test__/src/app/page.test.tsx
+++ __test__/src/app/page.test.tsx
@@ -1,34 +1,64 @@
 import { render, screen, waitFor, waitForElementToBeRemoved } from "@testing-library/react";
 import createWrapper from "@/testUtils/createWrapper";
-import Home from "@/src/app/page";
+import Todos from "@/src/components/todos";
+import useFetchTodos from "@/src/hooks/useFetchTodos";
 
-const todos = {
-  "todos": [
-    {
-      "id": "1",
-      "title": "Go shopping",
-      "done": false
-    },
-    {
-      "id": "2",
-      "title": "Walk the dog",
-      "done": false
-    },
-    {
-      "id": "f1a82176-9197-4c61-b9bd-201478facbaf",
-      "title": "Exercise",
-      "done": false
-    }
-  ]
-};
+jest.mock("../../../src/hooks/useFetchTodos");
 
-test("<Home /> - render loading state", async () => {
-  render(<Home />, {
-    wrapper: createWrapper()
+const todos = [
+  {
+    "id": "1",
+    "title": "Go shopping",
+    "done": false
+  },
+  {
+    "id": "2",
+    "title": "Walk the dog",
+    "done": false
+  },
+  {
+    "id": "f1a82176-9197-4c61-b9bd-201478facbaf",
+    "title": "Exercise",
+    "done": false
+  }
+];
+
+test("renders loading state", async () => {
+  (useFetchTodos as jest.Mock).mockReturnValue({
+    data: undefined,
+    isLoading: true,
+    isError: false,
+    error: null,
   });
-  await waitFor(() => screen.getByText("Todo-List-App"));
-  expect(screen.getByText("Loading...")).toBeInTheDocument();
-  await waitFor(() => waitForElementToBeRemoved(screen.queryByText("Loading...")));
+  render(<Todos />, {
+    wrapper: createWrapper(),
+  });
+  await screen.findByText(/loading/i);
+});
+
+test("renders error state", async () => {
+  (useFetchTodos as jest.Mock).mockReturnValue({
+    data: undefined,
+    isLoading: false,
+    isError: true,
+    error: new Error("Error"),
+  });
+  render(<Todos />, {
+    wrapper: createWrapper(),
+  });
+  await screen.findByText(/error/i);
+});
+
+test("renders success state", async () => {
+  (useFetchTodos as jest.Mock).mockReturnValue({
+    data: todos,
+    isLoading: false,
+    isError: false,
+    error: null,
+  });
+  render(<Todos />, {
+    wrapper: createWrapper(),
+  });
   expect(screen.getByText(/go shopping/i)).toBeInTheDocument();
   expect(screen.getByText(/walk the dog/i)).toBeInTheDocument();
   expect(screen.getByText(/exercise/i)).toBeInTheDocument();
